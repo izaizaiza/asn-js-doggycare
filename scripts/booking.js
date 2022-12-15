f8789f39e3c291933f8bd7ad811eec54c06d1af7
iza lumpio <iza.lumpio@edu.sait.ca>
2022-12-15 14:15:42 -0700

    fixed issue with cost and shortened code

--- scripts/booking.js
+++ scripts/booking.js
@@ -2,44 +2,124 @@
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-var daySelector = document.getElementsByClassName("day-selector");
-//make a variable for each day id
-var dayButtons = document.querySelectorAll("#monday","#tuesday","#wednesday","thursday","friday");
-console.log(dayButtons.classList);
-var costPerDay;
-var numOfDays;
-var calculatedCost = document.getElementById("calculated-cost").innerHTML;
-console.log(calculatedCost);
-var clearDays = document.getElementById("clear-button").innerHTML;
-console.log(clearDays);
+const mondayButn = document.getElementById("monday");
+const tuesdayButn = document.getElementById("tuesday");
+const wednesdayButn = document.getElementById("wednesday");
+const thursdayButn = document.getElementById("thursday");
+const fridayButn = document.getElementById("friday");
+
+const fullButn = document.getElementById("full");
+const halfButn = document.getElementById("half");
+const clearButn = document.getElementById("clear-button");
+
+let costPerDay=0;
+let numOfDays=0;
+
+var calculatedCost = document.getElementById("calculated-cost");
 
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-dayButtons.addEventListener("click", function(){
-    dayButtons.classList.add(" clicked");
-})
+//create function that remove all clicked and make function to apply click 
+function resetDayButns(){
+    mondayButn.classList.remove("clicked");
+    tuesdayButn.classList.remove("clicked");
+    wednesdayButn.classList.remove("clicked");
+    thursdayButn.classList.remove("clicked");
+    fridayButn.classList.remove("clicked");
+}
+function applyClicked(butn){
+    butn.classList.add("clicked");
+}
 
+function resetAndApplyClicked(butn){
+    resetDayButns();
+    applyClicked(butn);
+}
 
+//make function that uses the two functions for ANY day button
+function clickThisDay(event){
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target);
+    //make a local var for any day button
+    //const dayButn = event.target;
+    resetAndApplyClicked(event.target);
+}
+//use the two functions to each button:
+/*function onMondayButn(e){
+    e.preventDefault();
+    resetAndApplyClicked(wednesdayButn);
+    //console.log("mon clicked")
+}
+function onTuesdayButn(e){
+    e.preventDefault();
+    resetAndApplyClicked(tuesdayButn);
+}
+function onWednesdayButn(e){
+    e.preventDefault();
+    resetAndApplyClicked(wednesdayButn);
+}
+function onThursdayButn(e){
+    e.preventDefault();
+
+    resetAndApplyClicked(thursdayButn);
+}
+function onFridayButn(e){
+    e.preventDefault();
+    resetAndApplyClicked(fridayButn);
+}
+*/
+// apply the functions on each button when user clicks it
+mondayButn.onclick = clickThisDay;
+tuesdayButn.onclick = clickThisDay;
+wednesdayButn.onclick = clickThisDay;
+thursdayButn.onclick = clickThisDay;
+fridayButn.onclick = clickThisDay;
+
+//mondayButn.onclick = onMondayButn;
+//tuesdayButn.onclick = onTuesdayButn;
+//wednesdayButn.onclick = onWednesdayButn;
+//thursdayButn.onclick = onThursdayButn;
+//fridayButn.onclick = onFridayButn;
 
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
-
-
-
-
+function onClearDays(){
+    resetDayButns();
+    calculatedCost.innerHTML = 0;
+    halfButn.classList.remove("clicked");
+    applyClicked(fullButn);
+    //fullButn.classList.add("clicked");
+}
+clearButn.onclick = onClearDays;
 
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-
+function onHalfDay(){
+    costPerDay = 20;
+    fullButn.classList.remove("clicked");
+    applyClicked(halfButn);
+    calculate(costPerDay);
+}
 
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+function onFullDay(){
+    costPerDay = 35;
+    halfButn.classList.remove("clicked");
+    applyClicked(fullButn);
+    calculate(costPerDay);
+}
+
+
+halfButn.onclick = onHalfDay;
+fullButn.onclick = onFullDay;
 
 
 
@@ -47,5 +127,7 @@ dayButtons.addEventListener("click", function(){
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-
+function calculate(costPerDay){
+    calculatedCost.innerHTML = costPerDay;
+}
 
